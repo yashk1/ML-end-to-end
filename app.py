9d23ce7c201229efdecd2714944e19c4fc6a6df7
Yash Sharma <yash95kumar@gmail.com>
2023-08-26 14:16:46 -0700

    logger completed

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from housing.logger import logging
 
 app = Flask(__name__)
 
 @app.route("/",methods = ['GET','POST'])
 def index():
+    logging.info("Testing logging module")
     return "Starting ML project"
 
 if __name__ == '__main__':
